7c21c5b9a3664809126977ccd9ab65bd708ab186
funfunpluto <funfunpluto@gmail.com>
2018-12-04 18:19:44 -0500

    Changes to be committed:

---
+++ README.md
@@ -0,0 +1,3 @@
+# this is a directory for python scripts
+
+fiData: use python to extract financial data
---
+++ fiData/quantTrading.py
@@ -0,0 +1,11 @@
+#! /usr/local/bin/python3
+
+# https://www.learndatasci.com/tutorials/python-finance-part-2-intro-quantitative-trading-strategies/
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set(style='darkgrid', context='talk', palette='Dark2')
+data = pd.read_pickle(' 
