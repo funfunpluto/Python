b814169bff78819aa9b78802c4641e1ebff3cdd2
funfunpluto <funfunpluto>
2018-12-04 20:00:10 -0500

    Changes to be committed:

--- fiData/quantTrading.py
+++ fiData/quantTrading.py
@@ -8,4 +8,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set(style='darkgrid', context='talk', palette='Dark2')
-data = pd.read_pickle(' 
+
+#read saved pickle file and display the top 10 records
+data = pd.read_pickle('stock.pkl')
+print(data.head(10)) 
+
+short_rolling = data.rolling(window=20).mean()
+long_rolling = data.rolling(window=100).mean()
+print(long_rolling.tail())
+
+
